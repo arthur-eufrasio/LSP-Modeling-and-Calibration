9f3daa52bacf62e9d1668d2a90c2e81626aa839b
eufragod2909 <arthur.eufrasio.ufsc@gmail.com>
2026-08-18 23:08:02 +0200

    F

--- delete_files.bat
+++ delete_files.bat
@@ -1,37 +1,27 @@
 @echo off
-REM Script to clear specific directories while keeping .gitkeep files
+REM Script to clear all files from specific directories
 
 set "BASE_DIR=backend\"
 
 REM Clean cae directory
 echo Cleaning %BASE_DIR%\files\cae...
-for %%i in ("%BASE_DIR%\files\cae\*") do (
-    if /I not "%%~nxi"==".gitkeep" del /q "%%i"
-)
+del /q /f "%BASE_DIR%\files\cae\*"
 
 REM Clean inp directory
 echo Cleaning %BASE_DIR%\files\inp...
-for %%i in ("%BASE_DIR%\files\inp\*") do (
-    if /I not "%%~nxi"==".gitkeep" del /q "%%i"
-)
+del /q /f "%BASE_DIR%\files\inp\*"
 
 REM Clean job directory
 echo Cleaning %BASE_DIR%\files\job...
-for %%i in ("%BASE_DIR%\files\job\*") do (
-    if /I not "%%~nxi"==".gitkeep" del /q "%%i"
-)
+del /q /f "%BASE_DIR%\files\job\*"
 
 REM Clean data directory
 echo Cleaning %BASE_DIR%\data...
-for %%i in ("%BASE_DIR%\data\*") do (
-    if /I not "%%~nxi"==".gitkeep" del /q "%%i"
-)
+del /q /f "%BASE_DIR%\data\*"
 
 REM Clean log directory
 echo Cleaning %BASE_DIR%\log...
-for %%i in ("%BASE_DIR%\log\*") do (
-    if /I not "%%~nxi"==".gitkeep" del /q "%%i"
-)
+del /q /f "%BASE_DIR%\log\*"
 
 echo.
 echo Cleanup process finished successfully!
